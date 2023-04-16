35645c031692ba34f53412526b6d7b55555dc531
Drew Bredvick <dbredvick@gmail.com>
2023-04-16 14:49:42 -0500

    waveformComponent

--- app/opengraph-image.jsx
+++ app/opengraph-image.jsx
@@ -5,72 +5,72 @@ export const size = { width: 1200, height: 600 };
 export const alt = 'About this episode of Cashed.dev';
 export const contentType = 'image/png';
 
-// function randomBetween(min, max, seed = 1) {
-//     return () => {
-//         let rand = Math.sin(seed++) * 10000
-//         rand = rand - Math.floor(rand)
-//         return Math.floor(rand * (max - min + 1) + min)
-//     }
-// }
+function randomBetween(min, max, seed = 1) {
+    return () => {
+        let rand = Math.sin(seed++) * 10000
+        rand = rand - Math.floor(rand)
+        return Math.floor(rand * (max - min + 1) + min)
+    }
+}
 
-// export function Waveform(props) {
-//     let id = 'waveform'
-//     let bars = {
-//         total: 100,
-//         width: .4,
-//         gap: .2,
-//         minHeight: 40,
-//         maxHeight: 100,
-//     }
+export function Waveform(props) {
+    let id = 'waveform'
+    let bars = {
+        total: 100,
+        width: .4,
+        gap: .2,
+        minHeight: 40,
+        maxHeight: 100,
+    }
 
-//     let barHeights = Array.from(
-//         { length: bars.total },
-//         randomBetween(bars.minHeight, bars.maxHeight)
-//     )
+    let barHeights = Array.from(
+        { length: bars.total },
+        randomBetween(bars.minHeight, bars.maxHeight)
+    )
 
 
-//     return (
-//         <svg aria-hidden="true" {...props}>
-//             <defs>
-//                 <linearGradient id={`${id}-fade`} x1="0" x2="0" y1="0" y2="1">
-//                     <stop offset="40%" stopColor="white" />
-//                     <stop offset="100%" stopColor="black" />
-//                 </linearGradient>
-//                 <linearGradient id={`${id}-gradient`}>
-//                     <stop offset="0%" stopColor="#2DD4BF" />
-//                     <stop offset="50%" stopColor="#34D399" />
-//                     <stop offset="100%" stopColor="#4ADE80" />
-//                 </linearGradient>
-//                 <mask id={`${id}-mask`}>
-//                     <rect width="100%" height="100%" fill={`url(#${id}-pattern)`} />
-//                 </mask>
-//                 <pattern
-//                     id={`${id}-pattern`}
-//                     width={bars.total * bars.width + bars.total * bars.gap}
-//                     height="100%"
-//                     patternUnits="userSpaceOnUse"
-//                 >
-//                     {Array.from({ length: bars.total }, (_, index) => (
-//                         <rect
-//                             key={index}
-//                             width={bars.width}
-//                             height={`${barHeights[index]}%`}
-//                             x={bars.gap * (index + 1) + bars.width * index}
-//                             fill={`url(#${id}-fade)`}
-//                         />
-//                     ))}
-//                 </pattern>
-//             </defs>
-//             <rect
-//                 width="100%"
-//                 height="100%"
-//                 fill={`url(#${id}-gradient)`}
-//                 mask={`url(#${id}-mask)`}
-//                 opacity="0.25"
-//             />
-//         </svg>
-//     )
-// }
+    return (
+        <svg aria-hidden="true" {...props}>
+            <defs>
+                <linearGradient id={`${id}-fade`} x1="0" x2="0" y1="0" y2="1">
+                    <stop offset="40%" stopColor="white" />
+                    <stop offset="100%" stopColor="black" />
+                </linearGradient>
+                <linearGradient id={`${id}-gradient`}>
+                    <stop offset="0%" stopColor="#2DD4BF" />
+                    <stop offset="50%" stopColor="#34D399" />
+                    <stop offset="100%" stopColor="#4ADE80" />
+                </linearGradient>
+                <mask id={`${id}-mask`}>
+                    <rect width="100%" height="100%" fill={`url(#${id}-pattern)`} />
+                </mask>
+                <pattern
+                    id={`${id}-pattern`}
+                    width={bars.total * bars.width + bars.total * bars.gap}
+                    height="100%"
+                    patternUnits="userSpaceOnUse"
+                >
+                    {Array.from({ length: bars.total }, (_, index) => (
+                        <rect
+                            key={index}
+                            width={bars.width}
+                            height={`${barHeights[index]}%`}
+                            x={bars.gap * (index + 1) + bars.width * index}
+                            fill={`url(#${id}-fade)`}
+                        />
+                    ))}
+                </pattern>
+            </defs>
+            <rect
+                width="100%"
+                height="100%"
+                fill={`url(#${id}-gradient)`}
+                mask={`url(#${id}-mask)`}
+                opacity="0.25"
+            />
+        </svg>
+    )
+}
 
 export default async function og(req) {
     const feed = await parse('https://feeds.transistor.fm/cashed-dev');
@@ -123,7 +123,7 @@ export default async function og(req) {
             <img tw="w-[640px]" src={"https://cashed.dev/poster.png"} />
 
             <div tw="flex w-full flex-1 flex-wrap">
-                {/* <Waveform tw="absolute left-0 top-0 h-40 w-full" /> */}
+                <Waveform tw="absolute left-0 top-0 h-40 w-full" />
                 <h1 tw="ml-4 mt-48 text-5xl font-bold tracking-tight ">
                     {title}
                 </h1>
